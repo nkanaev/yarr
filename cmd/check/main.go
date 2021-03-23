e0009e4267a5be2add2d83f5e3cc7db01a5aba61
Nazar Kanaev <nkanaev@live.com>
2021-03-23 15:21:02 +0000

    feed checker tool

---
+++ cmd/check/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/nkanaev/yarr/src/feed"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("usage: <script> url")
+		return
+	}
+	url := os.Args[1]
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("failed to get url %s: %s", url, err)
+	}
+	feed, err := feed.Parse(res.Body)
+	if err != nil {
+		log.Fatalf("failed to parse feed: %s", err)
+	}
+	body, err := json.MarshalIndent(feed, "", "  ")
+	if err != nil {
+		log.Fatalf("failed to marshall feed: %s", err)
+	}
+	fmt.Println(string(body))
+}
