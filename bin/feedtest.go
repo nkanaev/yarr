1be79d922bc262c6bca3c34b0d1f8819ab150732
Nazar Kanaev <nkanaev@live.com>
2021-03-25 16:55:47 +0000

    feedtest: support local files

--- bin/feedtest.go
+++ bin/feedtest.go
@@ -3,24 +3,37 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/nkanaev/yarr/src/parser"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("usage: <script> url")
+		fmt.Println("usage: <script> [url|filepath]")
 		return
 	}
 	url := os.Args[1]
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatalf("failed to get url %s: %s", url, err)
+	var r io.Reader
+
+	if strings.HasPrefix(url, "http") {
+		res, err := http.Get(url)
+		if err != nil {
+			log.Fatalf("failed to get url %s: %s", url, err)
+		}
+		r = res.Body
+	} else {
+		var err error
+		r, err = os.Open(url)
+		if err != nil {
+			log.Fatalf("failed to open file: %s", err)
+		}
 	}
-	feed, err := parser.Parse(res.Body)
+	feed, err := parser.Parse(r)
 	if err != nil {
 		log.Fatalf("failed to parse feed: %s", err)
 	}
