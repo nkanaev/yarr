ac3689215087ceecb862efbd051cd653a9a6f2e2
Nazar Kanaev <nkanaev@live.com>
2021-03-30 12:23:10 +0100

    reader utility

--- bin/reader.go
+++ bin/reader.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"io"
+	"strings"
 
 	"github.com/nkanaev/yarr/src/reader"
 )
@@ -15,13 +17,23 @@ func main() {
 		return
 	}
 	url := os.Args[1]
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatalf("failed to get url %s: %s", url, err)
-	}
-	defer res.Body.Close()
+	var r io.Reader
 
-	content, err := reader.ExtractContent(res.Body)
+	if strings.HasPrefix(url, "http") {
+		res, err := http.Get(url)
+		if err != nil {
+			log.Fatalf("failed to get url %s: %s", url, err)
+		}
+		r = res.Body
+	} else {
+		var err error
+		r, err = os.Open(url)
+		if err != nil {
+			log.Fatalf("failed to open file: %s", err)
+		}
+	}
+
+	content, err := reader.ExtractContent(r)
 	if err != nil {
 		log.Fatalf("failed to extract content: %s", err)
 	}
