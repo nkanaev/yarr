9919d72be0c4ca2c82974976313a978b09733974
Nazar Kanaev <nkanaev@live.com>
2021-04-05 20:35:30 +0100

    more tests

--- src/storage/feed_test.go
+++ src/storage/feed_test.go
@@ -22,6 +22,35 @@ func TestReadFeed(t *testing.T) {
 	if db.GetFeed(100500) != nil {
 		t.Fatal("cannot get nonexistent feed")
 	}
+
+	feed1 := db.CreateFeed("feed 1", "", "http://example1.com", "http://example1.com/feed.xml", nil)
+	feed2 := db.CreateFeed("feed 2", "", "http://example2.com", "http://example2.com/feed.xml", nil)
+	feeds := db.ListFeeds()
+	if !reflect.DeepEqual(feeds, []Feed{*feed1, *feed2}) {
+		t.Fatalf("invalid feed list: %#v", feeds)
+	}
+}
+
+func TestUpdateFeed(t *testing.T) {
+	db := testDB()
+	feed1 := db.CreateFeed("feed 1", "", "http://example1.com", "http://example1.com/feed.xml", nil)
+	folder := db.CreateFolder("test")
+	icon := []byte("icon")
+
+	db.RenameFeed(feed1.Id, "newtitle")
+	db.UpdateFeedFolder(feed1.Id, &folder.Id)
+	db.UpdateFeedIcon(feed1.Id, &icon)
+
+	feed2 := db.GetFeed(feed1.Id)
+	if feed2.Title != "newtitle" {
+		t.Error("invalid title")
+	}
+	if feed2.FolderId == nil || *feed2.FolderId != folder.Id {
+		t.Error("invalid folder")
+	}
+	if !feed2.HasIcon || string(*feed2.Icon) != "icon" {
+		t.Error("invalid icon")
+	}
 }
 
 func TestDeleteFeed(t *testing.T) {
