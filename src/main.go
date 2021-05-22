c072783c42e4d0f8f99b5458d28e00e379ea9a3f
Nazar Kanaev <nkanaev@live.com>
2021-05-22 21:51:39 +0100

    remove extra underscore from env vars

--- src/main.go
+++ src/main.go
@@ -29,12 +29,12 @@ func main() {
 	var addr, db, authfile, certfile, keyfile, basepath, logfile string
 	var ver, open bool
 	flag.StringVar(&addr, "addr", opt("YARR_ADDR", "127.0.0.1:7070"), "address to run server on")
-	flag.StringVar(&authfile, "auth-file", opt("YARR_AUTH_FILE", ""), "path to a file containing username:password")
+	flag.StringVar(&authfile, "auth-file", opt("YARR_AUTHFILE", ""), "path to a file containing username:password")
 	flag.StringVar(&basepath, "base", opt("YARR_BASE", ""), "base path of the service url")
-	flag.StringVar(&certfile, "cert-file", opt("YARR_CERT_FILE", ""), "path to cert file for https")
-	flag.StringVar(&keyfile, "key-file", opt("YARR_KEY_FILE", ""), "path to key file for https")
+	flag.StringVar(&certfile, "cert-file", opt("YARR_CERTFILE", ""), "path to cert file for https")
+	flag.StringVar(&keyfile, "key-file", opt("YARR_KEYFILE", ""), "path to key file for https")
 	flag.StringVar(&db, "db", opt("YARR_DB", ""), "storage file path")
-	flag.StringVar(&logfile, "log-file", opt("YARR_LOG_FILE", ""), "path to log file to use instead of stdout")
+	flag.StringVar(&logfile, "log-file", opt("YARR_LOGFILE", ""), "path to log file to use instead of stdout")
 	flag.BoolVar(&ver, "version", false, "print application version")
 	flag.BoolVar(&open, "open", false, "open the server in browser")
 	flag.Parse()
