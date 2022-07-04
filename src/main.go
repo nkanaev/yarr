f030a4075b7bc742c1db60400bb3188a8ebbfd59
nkanaev <nazar.kanaev@innovid.com>
2022-07-04 16:03:50 +0100

    refactoring

--- src/main.go
+++ src/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -25,6 +27,21 @@ func opt(envVar, defaultValue string) string {
 		return value
 	}
 	return defaultValue
+}
+
+func parseAuthfile(authfile io.Reader) (username, password string, err error) {
+	scanner := bufio.NewScanner(authfile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) != 2 {
+			return "", "", fmt.Errorf("wrong syntax (expected `username:password`)")
+		}
+		username = parts[0]
+		password = parts[1]
+		break
+	}
+	return username, password, nil
 }
 
 func main() {
