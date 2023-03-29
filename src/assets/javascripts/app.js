a6579e53c0aebe3ed5d65d986f95ae9ef86666a7
Steve Lemuel <wlemuel@hotmail.com>
2023-03-29 02:21:19 +0000

    fix: change default light of auto theme to theme-light

--- src/assets/javascripts/app.js
+++ src/assets/javascripts/app.js
@@ -698,7 +698,7 @@ var vm = new Vue({
           "(prefers-color-scheme: dark)"
         ).matches
           ? "theme-night"
-          : "theme-sepia";
+          : "theme-light";
       } else {
         document.body.classList.value = "theme-" + this.theme.name;
       }
