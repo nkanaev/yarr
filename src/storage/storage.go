349c966c63ba1a213c837e9c7ced3ac6bbe8486e
Nazar Kanaev <nkanaev@live.com>
2024-06-21 10:56:48 +0100

    sqlite parameters

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -2,6 +2,9 @@ package storage
 
 import (
 	"database/sql"
+	"log"
+	"strings"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -10,13 +13,16 @@ type Storage struct {
 }
 
 func New(path string) (*Storage, error) {
+	if pos := strings.IndexRune(path, '?'); pos == -1 {
+        params := "_journal=WAL&_sync=NORMAL&_busy_timeout=5000&cache=shared"
+        log.Printf("opening db with params: %s", params)
+        path = path + "?" + params
+    }
+
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		return nil, err
 	}
-
-	// TODO: https://foxcpp.dev/articles/the-right-way-to-use-go-sqlite3
-	db.SetMaxOpenConns(1)
 
 	if err = migrate(db); err != nil {
 		return nil, err
