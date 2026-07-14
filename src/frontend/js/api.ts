5e9dfc5ace77d46cb084189966d9c176053679fd
nkanaev <nkanaev@live.com>
2026-07-14 22:23:58 +0100

    frontend: api typing fixes

--- src/frontend/js/api.ts
+++ src/frontend/js/api.ts
@@ -1,12 +1,15 @@
-var xfetch = function (resource, init) {
-  init = init || {};
-  if (["post", "put", "delete"].indexOf(init.method) !== -1) {
-    init["headers"] = init["headers"] || {};
-    init["headers"]["x-requested-by"] = "yarr";
+type Query = Record<string, any>;
+
+// TODO: proper types for object arguments
+
+var xfetch = function (resource: string, init: RequestInit = {}) {
+  if (["post", "put", "delete"].indexOf(init.method || "") !== -1) {
+    init["headers"] = new Headers(init["headers"]);
+    init["headers"].set("x-requested-by", "yarr");
   }
   return fetch(resource, init);
 };
-var api = function (method, endpoint, data) {
+var api = function (method: string, endpoint: string, data?: object) {
   var headers = { "Content-Type": "application/json" };
   return xfetch(endpoint, {
     method: method,
@@ -15,11 +18,11 @@ var api = function (method, endpoint, data) {
   });
 };
 
-var json = function (res) {
+var json = function (res: Response) {
   return res.json();
 };
 
-var param = function (query) {
+function param(query: Query) {
   if (!query) return "";
   return (
     "?" +
@@ -29,23 +32,23 @@ var param = function (query) {
       })
       .join("&")
   );
-};
+}
 
 export default {
   feeds: {
     list() {
       return api("get", "./api/feeds").then(json);
     },
-    create(data) {
+    create(data: object) {
       return api("post", "./api/feeds", data).then(json);
     },
-    update(id, data) {
+    update(id: number, data: object) {
       return api("put", "./api/feeds/" + id, data);
     },
-    delete(id) {
+    delete(id: number) {
       return api("delete", "./api/feeds/" + id);
     },
-    list_items(id) {
+    list_items(id: number) {
       return api("get", "./api/feeds/" + id + "/items").then(json);
     },
     refresh() {
@@ -59,30 +62,30 @@ export default {
     list() {
       return api("get", "./api/folders").then(json);
     },
-    create(data) {
+    create(data: object) {
       return api("post", "./api/folders", data).then(json);
     },
-    update(id, data) {
+    update(id: number, data: object) {
       return api("put", "./api/folders/" + id, data);
     },
-    delete(id) {
+    delete(id: number) {
       return api("delete", "./api/folders/" + id);
     },
-    list_items(id) {
+    list_items(id: number) {
       return api("get", "./api/folders/" + id + "/items").then(json);
     },
   },
   items: {
-    get(id) {
+    get(id: number) {
       return api("get", "./api/items/" + id).then(json);
     },
-    list(query) {
+    list(query: Query) {
       return api("get", "./api/items" + param(query)).then(json);
     },
-    update(id, data) {
+    update(id: number, data: object) {
       return api("put", "./api/items/" + id, data);
     },
-    mark_read(query) {
+    mark_read(query: Query) {
       return api("put", "./api/items" + param(query));
     },
   },
@@ -90,14 +93,14 @@ export default {
     get() {
       return api("get", "./api/settings").then(json);
     },
-    update(data) {
+    update(data: object) {
       return api("put", "./api/settings", data);
     },
   },
   status() {
     return api("get", "./api/status").then(json);
   },
-  upload_opml(form) {
+  upload_opml(form: HTMLFormElement) {
     return xfetch("./opml/import", {
       method: "post",
       body: new FormData(form),
@@ -106,7 +109,7 @@ export default {
   logout() {
     return api("post", "./logout");
   },
-  crawl(url) {
+  crawl(url: string) {
     return api("get", "./page?url=" + encodeURIComponent(url)).then(json);
   },
 };
