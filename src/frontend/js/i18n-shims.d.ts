2d2a26d4231fb60b05401aff2e72a53a69800e73
nkanaev <nkanaev@live.com>
2026-07-19 00:34:59 +0100

    frontend: i18n type annotations

--- src/frontend/js/i18n-shims.d.ts
+++ src/frontend/js/i18n-shims.d.ts
@@ -1,12 +1,12 @@
 import "vue";
-import type { Lang } from "./i18n";
+import type { Lang, TranslationKey } from "./i18n";
 import { FluentVariable } from "@fluent/bundle";
 
 declare module "vue" {
   interface ComponentCustomProperties {
     $setLang: (lang: Lang) => void;
     $t: (
-      code: string,
+      code: TranslationKey,
       args?: Record<string, FluentVariable>,
     ) => string | undefined;
   }
