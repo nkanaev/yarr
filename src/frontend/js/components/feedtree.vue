85e7664dfd9ff8c20b089de45b003dddb2b767db
nkanaev <nkanaev@live.com>
2026-07-23 15:31:21 +0100

    frontend: rewrite feed column

--- src/frontend/js/components/feedtree.vue
+++ src/frontend/js/components/feedtree.vue
@@ -1,22 +1,22 @@
 <template>
-  <div>
+  <div class="d-flex flex-column gap-1">
     <div
-      class="selectgroup"
+      class="c-listitem d-flex align-items-center user-select-none gap-2"
       role="radio"
       :aria-checked="modelValue === ''"
       @click="$emit('update:modelValue', '')">
-      <div class="selectgroup-label d-flex align-items-center w-100">
-        <v-icon class="me-2" name="layers" />
-        <span class="flex-fill text-start text-truncate" v-if="filterSelected == 'unread'">{{
-          $t("all_unread")
-        }}</span>
-        <span class="flex-fill text-start text-truncate" v-if="filterSelected == 'starred'">{{
-          $t("all_starred")
-        }}</span>
-        <span class="flex-fill text-start text-truncate" v-if="filterSelected == ''">{{
-          $t("all_feeds")
-        }}</span>
-        <span class="counter text-end">{{ filterSelected ? stats.total[filterSelected] : "" }}</span>
+      <div class="flex-shrink-0">
+        <v-icon class="flex-shrink-0" name="layers" />
+      </div>
+      <div class="flex-grow-1 min-w-0 text-truncate">
+        {{
+          { unread: $t("all_unread"), starred: $t("all_starred"), "": $t("all_feeds") }[
+            filterSelected
+          ]
+        }}
+      </div>
+      <div class="flex-shrink-0" v-if="filterSelected">
+        <span class="counter text-end">{{ stats.total[filterSelected] }}</span>
       </div>
     </div>
     <template
@@ -24,37 +24,52 @@
       :key="node.type === 'folder' ? 'folder:' + node.folder.id : 'feed:' + node.feed.id">
       <template v-if="node.type === 'folder'">
         <div
-          class="selectgroup mt-1"
+          class="c-listitem d-flex align-items-center user-select-none gap-2"
           role="radio"
           :aria-checked="modelValue === 'folder:' + node.folder.id"
           @click="$emit('update:modelValue', 'folder:' + node.folder.id)">
-          <div class="selectgroup-label d-flex align-items-center w-100">
-            <div @click.stop="$emit('toggle-folder', node.folder)" class="m-n1 p-1">
+          <div class="flex-shrink-0">
+            <div @click.stop="$emit('toggle-folder', node.folder)" class="p-2 m-n2">
               <v-icon
-                class="me-2"
+                class="flex-shrink-0"
                 :class="{ expanded: node.folder.is_expanded }"
                 name="chevron-right" />
             </div>
-            <span class="flex-fill text-start text-truncate">{{ node.folder.title }}</span>
-            <span class="counter text-end">{{ filterSelected ? stats.folders[node.folder.id]?.[filterSelected] : "" }}</span>
+          </div>
+          <div class="flex-grow-1 min-w-0 text-truncate">
+            {{ node.folder.title }}
+          </div>
+          <div class="flex-shrink-0" v-if="filterSelected">
+            <span class="counter text-end">{{
+              stats.folders[node.folder.id]?.[filterSelected]
+            }}</span>
           </div>
         </div>
-        <div v-show="node.folder.is_expanded" class="mt-1 ps-3">
+        <div
+          v-if="node.folder.is_expanded && node.feeds.length > 0"
+          class="d-flex flex-column gap-1 ps-3">
           <div
-            class="selectgroup"
+            v-for="feedNode in node.feeds"
+            :key="'feed:' + feedNode.feed.id"
+            class="c-listitem d-flex align-items-center user-select-none gap-2"
             role="radio"
             :aria-checked="modelValue === 'feed:' + feedNode.feed.id"
-            @click="$emit('update:modelValue', 'feed:' + feedNode.feed.id)"
-            v-for="feedNode in node.feeds">
-            <div class="selectgroup-label d-flex align-items-center w-100">
-              <v-icon class="me-2" name="rss" v-if="!feedNode.feed.icon" />
-              <span class="icon me-2" v-else
-                ><img :src="feedNode.feed.icon" alt="" loading="lazy"
-              /></span>
-              <span class="flex-fill text-start text-truncate">{{ feedNode.feed.title }}</span>
-              <span class="counter text-end">{{ filterSelected ? stats.feeds[feedNode.feed.id]?.[filterSelected] : "" }}</span>
+            @click="$emit('update:modelValue', 'feed:' + feedNode.feed.id)">
+            <div class="flex-shrink-0">
+              <v-icon class="flex-shrink-0" name="rss" v-if="!feedNode.feed.icon" />
+              <span class="icon" v-else>
+                <img :src="feedNode.feed.icon" alt="" loading="lazy" />
+              </span>
+            </div>
+            <div class="flex-grow-1 min-w-0 text-truncate">
+              {{ feedNode.feed.title }}
+            </div>
+            <div class="flex-shrink-0">
+              <span class="counter text-end" v-if="filterSelected">{{
+                stats.feeds[feedNode.feed.id]?.[filterSelected]
+              }}</span>
               <v-icon
-                class="flex-shrink-0 mx-2"
+                class="flex-shrink-0"
                 :title="feedErrors[feedNode.feed.id]"
                 v-if="!filterSelected && feedErrors[feedNode.feed.id]"
                 name="alert-circle" />
@@ -64,19 +79,25 @@
       </template>
       <template v-else-if="node.type === 'feed'">
         <div
-          class="selectgroup"
+          class="c-listitem d-flex align-items-center user-select-none gap-2"
           role="radio"
           :aria-checked="modelValue === 'feed:' + node.feed.id"
           @click="$emit('update:modelValue', 'feed:' + node.feed.id)">
-          <div class="selectgroup-label d-flex align-items-center w-100">
-            <v-icon class="me-2" name="rss" v-if="!node.feed.icon" />
-            <span class="icon me-2" v-else
-              ><img :src="node.feed.icon" alt="" loading="lazy"
-            /></span>
-            <span class="flex-fill text-start text-truncate">{{ node.feed.title }}</span>
-            <span class="counter text-end">{{ filterSelected ? stats.feeds[node.feed.id]?.[filterSelected] : "" }}</span>
+          <div class="flex-shrink-0">
+            <v-icon class="flex-shrink-0" name="rss" v-if="!node.feed.icon" />
+            <span class="icon" v-else>
+              <img :src="node.feed.icon" alt="" loading="lazy" />
+            </span>
+          </div>
+          <div class="flex-grow-1 min-w-0 text-truncate">
+            {{ node.feed.title }}
+          </div>
+          <div class="flex-shrink-0">
+            <span class="counter text-end" v-if="filterSelected">{{
+              stats.feeds[node.feed.id]?.[filterSelected]
+            }}</span>
             <v-icon
-              class="flex-shrink-0 mx-2"
+              class="flex-shrink-0"
               :title="feedErrors[node.feed.id]"
               v-if="!filterSelected && feedErrors[node.feed.id]"
               name="alert-circle" />
@@ -109,7 +130,7 @@ export default defineComponent({
   props: {
     tree: { type: Array as PropType<FeedTreeNode[]>, required: true },
     modelValue: { type: String, required: true },
-    filterSelected: { type: String as PropType<'' | 'unread' | 'starred'>, required: true },
+    filterSelected: { type: String as PropType<"" | "unread" | "starred">, required: true },
     stats: {
       type: Object as PropType<{
         feeds: Record<number, { unread: number; starred: number }>;
@@ -121,6 +142,5 @@ export default defineComponent({
     feedErrors: { type: Object as PropType<Record<number, string>>, required: true },
   },
   emits: ["update:modelValue", "toggle-folder"],
-  computed: {},
 });
 </script>
