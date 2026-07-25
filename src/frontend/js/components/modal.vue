a6a6ea01e519f7f0c9d5ed6af75caedb1d9955c0
nkanaev <nkanaev@live.com>
2026-07-25 15:22:38 +0100

    frontend: redesign modal

--- src/frontend/js/components/modal.vue
+++ src/frontend/js/components/modal.vue
@@ -1,13 +1,7 @@
 <template>
-  <div class="modal custom-modal" tabindex="-1" v-if="$props.open">
-    <div class="modal-dialog">
-      <div class="modal-content" ref="content">
-        <div class="modal-body">
-          <slot v-if="$props.open"></slot>
-        </div>
-      </div>
-    </div>
-  </div>
+  <dialog ref="dialog" @close="$emit('hide')">
+    <slot></slot>
+  </dialog>
 </template>
 
 <script lang="ts">
@@ -15,27 +9,12 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   props: ["open"],
-  data() {
-    return { opening: false };
-  },
+  emits: ["hide"],
   watch: {
-    open(newVal) {
-      if (newVal) {
-        this.opening = true;
-        document.addEventListener("click", this.handleClick);
-      } else {
-        document.removeEventListener("click", this.handleClick);
-      }
-    },
-  },
-  methods: {
-    handleClick(e: Event) {
-      const target = e.target as HTMLElement;
-      if (this.opening) {
-        this.opening = false;
-        return;
-      }
-      if (target.closest(".modal-content") == null) this.$emit("hide");
+    open(open) {
+      const el = this.$refs.dialog as HTMLDialogElement;
+      if (open) el.showModal();
+      else el.close();
     },
   },
 });
