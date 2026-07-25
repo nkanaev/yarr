b5a785f1454fa729703a18618f9d8c8638a0e555
nkanaev <nkanaev@live.com>
2026-07-25 16:27:48 +0100

    frontend: create shortcuts component

---
+++ src/frontend/js/components/shortcuts.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <p class="cursor-default">
+      <b>{{ $t("keyboard_shortcuts") }}</b>
+    </p>
+    <div class="d-flex flex-column mt-2">
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>1</kbd> <kbd>2</kbd> <kbd>3</kbd></div>
+        <div class="w-75">{{ $t("kb_show_filters") }}</div>
+      </div>
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>/</kbd></div>
+        <div class="w-75">{{ $t("kb_focus_search") }}</div>
+      </div>
+
+      <div class="py-3"></div>
+
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>j</kbd> <kbd>k</kbd></div>
+        <div class="w-75">{{ $t("kb_next_prev_article") }}</div>
+      </div>
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>l</kbd> <kbd>h</kbd></div>
+        <div class="w-75">{{ $t("kb_next_prev_feed") }}</div>
+      </div>
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>q</kbd></div>
+        <div class="w-75">{{ $t("kb_close_article") }}</div>
+      </div>
+
+      <div class="py-3"></div>
+
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>R</kbd></div>
+        <div class="w-75">{{ $t("kb_mark_all_read") }}</div>
+      </div>
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>r</kbd></div>
+        <div class="w-75">{{ $t("kb_mark_read") }}</div>
+      </div>
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>s</kbd></div>
+        <div class="w-75">{{ $t("kb_mark_starred") }}</div>
+      </div>
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>o</kbd></div>
+        <div class="w-75">{{ $t("kb_open_link") }}</div>
+      </div>
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>i</kbd></div>
+        <div class="w-75">{{ $t("kb_read_here") }}</div>
+      </div>
+      <div class="d-flex py-1">
+        <div class="w-25"><kbd>f</kbd> <kbd>b</kbd></div>
+        <div class="w-75">{{ $t("kb_scroll_content") }}</div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/frontend/js/pages/App.vue
+++ src/frontend/js/pages/App.vue
@@ -565,64 +565,7 @@
           </button>
         </form>
       </div>
-      <div v-else-if="settings == 'shortcuts'">
-        <p class="cursor-default">
-          <b>{{ $t("keyboard_shortcuts") }}</b>
-        </p>
-
-        <div class="d-flex flex-column mt-2">
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>1</kbd> <kbd>2</kbd> <kbd>3</kbd></div>
-            <div class="w-75">{{ $t("kb_show_filters") }}</div>
-          </div>
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>/</kbd></div>
-            <div class="w-75">{{ $t("kb_focus_search") }}</div>
-          </div>
-
-          <div class="py-3"></div>
-
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>j</kbd> <kbd>k</kbd></div>
-            <div class="w-75">{{ $t("kb_next_prev_article") }}</div>
-          </div>
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>l</kbd> <kbd>h</kbd></div>
-            <div class="w-75">{{ $t("kb_next_prev_feed") }}</div>
-          </div>
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>q</kbd></div>
-            <div class="w-75">{{ $t("kb_close_article") }}</div>
-          </div>
-
-          <div class="py-3"></div>
-
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>R</kbd></div>
-            <div class="w-75">{{ $t("kb_mark_all_read") }}</div>
-          </div>
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>r</kbd></div>
-            <div class="w-75">{{ $t("kb_mark_read") }}</div>
-          </div>
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>s</kbd></div>
-            <div class="w-75">{{ $t("kb_mark_starred") }}</div>
-          </div>
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>o</kbd></div>
-            <div class="w-75">{{ $t("kb_open_link") }}</div>
-          </div>
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>i</kbd></div>
-            <div class="w-75">{{ $t("kb_read_here") }}</div>
-          </div>
-          <div class="d-flex py-1">
-            <div class="w-25"><kbd>f</kbd> <kbd>b</kbd></div>
-            <div class="w-75">{{ $t("kb_scroll_content") }}</div>
-          </div>
-        </div>
-      </div>
+      <v-shortcuts v-else-if="settings == 'shortcuts'" />
     </v-modal>
   </div>
 </template>
@@ -634,6 +577,7 @@ import { scrollto, debounce, debounceMixin } from "../utils";
 import drag from "../components/drag.vue";
 import dropdown from "../components/dropdown.vue";
 import modal from "../components/modal.vue";
+import shortcuts from "../components/shortcuts.vue";
 import relativeTime from "../components/relative-time.vue";
 import icon from "../components/icon.vue";
 import feedTree from "../components/feedtree.vue";
@@ -671,6 +615,7 @@ export default defineComponent({
     "v-drag": drag,
     "v-dropdown": dropdown,
     "v-modal": modal,
+    "v-shortcuts": shortcuts,
     "v-relative-time": relativeTime,
     "v-icon": icon,
     "v-feedtree": feedTree,
